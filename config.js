46ae703ef6f8190d8bf596ee9963eb5806603df7
Andrew Varnerin <ffac06@gmail.com>
2013-06-09 11:04:53 -0400

    fixed fs.exists problem (async duh)

--- config.js
+++ config.js
@@ -7,7 +7,8 @@ if (typeof args[0] !== 'undefined'){
     environment = args[0];
 }
 var filePath = "./config/" + environment + ".json";
-if (!fs.exists(filePath)){
+
+if (!fs.existsSync(filePath)){
     filePath = "./config/config.json";
 }
 var config = JSON.parse(fs.readFileSync(filePath));

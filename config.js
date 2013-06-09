a2856010f59d2c9017c74b3423ba6a7f61b198da
Andrew Varnerin <ffac06@gmail.com>
2013-06-08 22:10:45 -0400

    read environment from args

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 var fs = require('fs');
 
-var config = JSON.parse(fs.readFileSync("./config/development.json"));
+var args = require('optimist').argv._;
+
+var environment = 'development';
+if (typeof args[0] !== 'undefined'){
+    environment = args[0];
+}
+var config = JSON.parse(fs.readFileSync("./config/" + environment + ".json"));
 
 module.exports = config;
